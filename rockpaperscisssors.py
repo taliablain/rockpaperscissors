6b84d5421cfbcbd36d05e61da0476a11828c7496
Talia Blain <tb1126@york.ac.uk>
2022-03-12 13:43:27 +0000

    put the entire code into functions that work

--- rockpaperscisssors.py
+++ rockpaperscisssors.py
@@ -2,54 +2,66 @@ import random
 import sys
 from time import sleep
 
-computer_choice = random.randint(0,2)
+def computer_choice():
+    computer_choice = random.randint(0,2)
 
-if computer_choice == 0:
-        computer_choice = 'rock'
+    if computer_choice == 0:
+            computer_choice = 'rock'
 
-if computer_choice == 1:
-        computer_choice = 'paper'
+    if computer_choice == 1:
+            computer_choice = 'paper'
 
-if computer_choice == 2:
+    if computer_choice == 2:
         computer_choice = 'scissors'
+    return computer_choice
 
-
-def decide_winner(choice, computer_choice):
-    print('you selected: %s' % choice)
-    print('computer selecting...')
-    sleep(1)
-    print('computer selected: %s' % computer_choice)
-
-    if choice == computer_choice:
-        print(f'both players selected {choice}. it''s a draw!')
-
-    elif choice == 'rock':
-        if computer_choice == 'scissors':
-            print('rock beats scissors! you win!')
-        else:
-            print('paper beats rock! you lose!')
-    elif choice == 'paper':
-        if computer_choice == 'rock':
-            print('paper beats rock! you win!')
-        else:
-            print('scissors beats paper! you lose!')
-    elif choice == 'scissors':
-        if computer_choice == 'paper':
-            print('scissors beats paper! you win!')
-        else:
-            print('rock beats scissors! you lose!')
-
-def rps_game():
+def user_choice():
     choice = input('R, P or S? :  ')
     choice = choice.upper()
     if choice == 'R':
         choice = 'rock'
+        return choice
     if choice == 'S':
         choice = 'scissors'
+        return choice
     if choice == 'P':
         choice = 'paper'
+        return choice
 
-    decide_winner(choice, computer_choice)
+
+def decide_winner():
+    choice = user_choice()
+    computer = computer_choice()
+    print('you selected: %s' % choice)
+    print('computer selecting...')
+    sleep(1)
+    print('computer selected: %s' % computer)
+
+    if choice == computer:
+        print(f'both players selected {choice}. it''s a draw!')
+
+    elif choice == 'rock':
+        if computer == 'scissors':
+            print('rock beats scissors! you win!')
+        else:
+            print('paper beats rock! you lose!')
+    elif choice == 'paper':
+        if computer == 'rock':
+            print('paper beats rock! you win!')
+        else:
+            print('scissors beats paper! you lose!')
+    elif choice == 'scissors':
+        if computer == 'paper':
+            print('scissors beats paper! you win!')
+        else:
+            print('rock beats scissors! you lose!')
+
+
+
+def rps_game():
+    decide_winner()
+
+
 
     while True:
         replay = input('play again? (y/n): ')
